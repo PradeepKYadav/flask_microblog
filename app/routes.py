acef8680f9845331f031f72449560a16d8360e97
PradeepKYadav <pradeep10138@outlook.com>
2018-07-04 22:45:39 +0530

    Initial commit - Flask app with Sqlite DB

---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Pradeep'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in India!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Avengers 4 was awesome!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me{}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
